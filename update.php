81841543677571cc6b38215ffc9fbedefbfc1185
Thomas Skerbis <thomas.skerbis@mac.com>
2022-05-26 21:46:15 +0200

    create table

---
+++ update.php
@@ -0,0 +1,9 @@
+<?php 
+rex_sql_table::get(rex::getTable('articlepresets_profiles'))
+    ->ensureColumn(new rex_sql_column('id', 'int(11) unsigned', false, null, 'auto_increment'))
+    ->ensureColumn(new rex_sql_column('name', 'varchar(30)', false, ''))
+    ->ensureColumn(new rex_sql_column('categories', 'mediumtext'))
+    ->ensureColumn(new rex_sql_column('templates', 'varchar(255)'))
+    ->ensureColumn(new rex_sql_column('articlereference', 'int(10) unsigned'))
+    ->setPrimaryKey('id')
+    ->ensure();
